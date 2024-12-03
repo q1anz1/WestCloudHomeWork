88197417b9dad37fc6220edcddadf26d1186d8e0
q1anz1 <qianz0w0@outlook.com>
2024-12-03 09:27:31 +0800

    创建仓库

---
+++ src/main/java/qianz/westcloud/WestcloudApplication.java
@@ -0,0 +1,13 @@
+package qianz.westcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WestcloudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WestcloudApplication.class, args);
+    }
+
+}
---
+++ src/test/java/qianz/westcloud/WestcloudApplicationTests.java
@@ -0,0 +1,13 @@
+package qianz.westcloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WestcloudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
